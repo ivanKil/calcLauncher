80b9fc4c742714d794b99dcbe174fd98b3d20d9e
kusch ivan <i-kush@mail.ru>
2021-04-26 11:59:27 +0700

    Запуск калькулятора

--- app/src/main/java/eva/android/calculatorlauncher/MainActivity.java
+++ app/src/main/java/eva/android/calculatorlauncher/MainActivity.java
@@ -14,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
+    private void init(){
+        Button runEcho = findViewById(R.id.but_launch);
+        runEcho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("calculator://intent");
+                Intent runEchoIntent = new Intent(Intent.ACTION_VIEW, uri);
+                runEchoIntent.putExtra("TEXT", "342.23+121");
+                startActivity(runEchoIntent);
+            }
+        });
+    }
 }
